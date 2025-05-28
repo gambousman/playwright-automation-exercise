f14a36b5711e79c4d140c311c37af3d6fa5a492e
Gambo Usman <gamboyusuf101@gmail.com>
2025-05-28 21:55:47 +0100

    update

--- pageObjects/ContactUsPage.js
+++ pageObjects/ContactUsPage.js
@@ -49,9 +49,9 @@ class ContactUsPage {
   async submitForm(){
     await this.submitButton.click()
   }
-  // async validateSuccessMessage() {
-  //   await expect(this.successMessage).toHaveText("")
-  // }
+  async validateSuccessMessage() {
+    await expect(this.successMessage).toContainText('Success! Your details have been submitted successfully.')
+  }
     async clickHomeLink() {
         await this.homeLink.click()
     }
